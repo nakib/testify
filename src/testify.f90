559acbb70ff4255b469d328df8777b9c922cb5fc
Nakib Haider <nakib.haider.protik@gmail.com>
2023-01-17 14:57:12 +0100

    Added copyright notice.

--- src/testify.f90
+++ src/testify.f90
@@ -1,5 +1,20 @@
+! Copyright (C) 2023 Nakib Haider Protik <nakib.haider.protik@gmail.com>
+! This file is part of testify <https://github.com/nakib/testify>.
+!
+! testify is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! testify is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with testify. If not, see <http://www.gnu.org/licenses/>.
 module testify_m
-
+  
   use iso_fortran_env, only : &
        i8 => int8, i16 => int16, i32 => int32, i64 => int64, &
        r32 => real32, r64 => real64, r128 => real128
